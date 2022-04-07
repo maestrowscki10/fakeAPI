d3b0a30b50a2d4d2910712e33fb57e8ad2155dfc
maestrowscki10 <abdulhansen.ah@gmail.com>
2022-04-07 15:54:24 +0300

    many changes

--- app.js
+++ app.js
@@ -1,8 +1,28 @@
-class EasyHTTP{
+class easyHTTP {
 get(url){
+return new Promise((resolve,reject)=>{
     fetch(url)
     .then(res =>res.json())
-    .then(data =>console.log(data))
-    .catch(err => console.log(err))
+    .then(data =>resolve (data))
+    .catch(err => reject(err))
+})
+
+    
 }
-}
+post(url,data){
+    return new Promise((resolve,reject)=>{
+        fetch(url,{
+            method:'POST',
+            Headers:{
+                'contenttype':'app/json'  
+            },  body: JSON.stringify(DataTransferItemList)
+        } )
+        .then(res =>res.json())
+        .then(data =>resolve (data))
+        .catch(err => reject(err))
+    })
+    
+        
+    }
+    }
+
--- app1.js
+++ app1.js
@@ -1,4 +1,19 @@
-const http = EasyHTTP
-const users = http.length('https://jsonplaceholder.typicode.com/users')
-  .then(data=>console.log(data))
-  .catch(err=>console.log(err))
+
+
+
+const http = easyHTTP
+// //get
+// http.length('https://jsonplaceholder.typicode.com/users')
+//   .then(data=>console.log(data))
+//   .catch(err=>console.log(err))
+
+
+const data ={
+    name:'rahim', 
+    username:'maestrowscki10'
+
+}
+
+http.post('https://jsonplaceholder.typicode.com/users',data)
+ .then(data=>console.log(data))
+   .catch(err=>console.log(err))
